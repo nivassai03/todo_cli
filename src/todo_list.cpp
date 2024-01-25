eace743e9c4d0e5cc6dbc223dfb0260d42044849
Nivas Pvs <sainivaspvs@gmail.com>
2024-01-25 21:30:32 +0530

    Display no tasks if task list is empty

--- src/todo_list.cpp
+++ src/todo_list.cpp
@@ -29,39 +29,45 @@ Component TList::generate_todolist() {
   m_map_todo.clear();
   m_todo_list = m_tododb->getTodos(get_date_string_format(m_ymd()));
   Components cur_todos;
-  for (auto j_object : m_todo_list) {
-    Todo t;
-    t.m_label = j_object["label"];
-    t.m_is_done = j_object["is_done"];
-    t.m_deadline = m_ymd();
-    m_map_todo.insert(std::make_pair(t.m_label, t));
-    CheckboxOption opt = CheckboxOption();
-    opt.transform = [&](const EntryState &s) {
-      auto prefix = text(s.state ? "[X]" : "[ ]");
-      auto ts = text(s.label);
-      if (s.active) {
-        ts |= bold;
-      }
-      if (s.state) {
-        prefix |= color(Color::Green);
-        ts |= strikethrough | color(Color::Green);
-      }
-      if (!s.state) {
-        prefix = text(s.state ? "[X]" : "[ ]");
-        ts = text(s.label);
-      }
-      return hbox({emptyElement() | size(WIDTH, EQUAL, 1), prefix,
-                   emptyElement() | size(WIDTH, EQUAL, 3), ts,
-                   emptyElement() | size(WIDTH, EQUAL, 35)}) |
-             size(HEIGHT, EQUAL, 1) | border;
-    };
-    opt.on_change = [=, this] {
-      updateTodo(get_date_string_format(m_map_todo.at(t.m_label).m_deadline),
-                 m_map_todo.at(t.m_label).m_label,
-                 m_map_todo.at(t.m_label).m_is_done);
-    };
-    cur_todos.push_back(Checkbox(&m_map_todo.at(t.m_label).m_label,
-                                 &m_map_todo.at(t.m_label).m_is_done, opt));
+  if (m_todo_list.empty()) {
+      auto empty_tasks_label = text("Hurray ! No Tasks For Today") | center | bold | bgcolor(Color::Black) |
+                        color(Color::Blue);
+      cur_todos.push_back(Renderer([=, this] { return empty_tasks_label; }));
+  } else {
+    for (auto j_object : m_todo_list) {
+      Todo t;
+      t.m_label = j_object["label"];
+      t.m_is_done = j_object["is_done"];
+      t.m_deadline = m_ymd();
+      m_map_todo.insert(std::make_pair(t.m_label, t));
+      CheckboxOption opt = CheckboxOption();
+      opt.transform = [&](const EntryState &s) {
+        auto prefix = text(s.state ? "[X]" : "[ ]");
+        auto ts = text(s.label);
+        if (s.active) {
+          ts |= bold;
+        }
+        if (s.state) {
+          prefix |= color(Color::Green);
+          ts |= strikethrough | color(Color::Green);
+        }
+        if (!s.state) {
+          prefix = text(s.state ? "[X]" : "[ ]");
+          ts = text(s.label);
+        }
+        return hbox({emptyElement() | size(WIDTH, EQUAL, 1), prefix,
+                     emptyElement() | size(WIDTH, EQUAL, 3), ts,
+                     emptyElement() | size(WIDTH, EQUAL, 35)}) |
+               size(HEIGHT, EQUAL, 1) | border;
+      };
+      opt.on_change = [=, this] {
+        updateTodo(get_date_string_format(m_map_todo.at(t.m_label).m_deadline),
+                   m_map_todo.at(t.m_label).m_label,
+                   m_map_todo.at(t.m_label).m_is_done);
+      };
+      cur_todos.push_back(Checkbox(&m_map_todo.at(t.m_label).m_label,
+                                   &m_map_todo.at(t.m_label).m_is_done, opt));
+    }
   }
   auto todo_list_component = Container::Vertical(cur_todos);
   return todo_list_component;
